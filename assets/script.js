ca082623e719cb2ded856047ac7bba4bddf9efd4
Jonah Milner <jonahmilner86@gmail.com>
2022-02-11 19:30:15 -0700

    started adding questions

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,52 @@ let timerDisplay = document.createElement("p")
 
 
 const timer = () => {
-    let countdown = 60
-    console.log('is work')
+    timerDisplay.textContent("60")
 }
 
-beginQuiz.addEventListener("click", timer());
+beginQuiz.addEventListener("click", timer(60));
+
+
+const quizQuestions = [
+    question1 = 
+    {
+         question: `On which of the following albums did the song "Houses Of The Holy" appear?`,
+         a: `Houses Of The Holy`,
+         b: `Physical Graffiti`,
+         c: `Led Zeppelin IV`,
+         d: `Presence`,
+         correct: `b`
+    },
+    question2 = 
+    {
+        question: `What color was the umbrella in the song "Living Loving Maid"?`,
+        a: `Blue`,
+        b: `Red`,
+        c: `Purple`,
+        d: `Green`,
+        correct: `c`
+    },
+    question3 = 
+    {
+        question: `What do the seasons of emotion do?`,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        correct: ``
+    },
+    question4 = 
+    {
+        question:``,
+        a: ``,
+        b: ``,
+        c: ``,
+        d: ``,
+        correct: ``
+    }
+        
+
+    
+
+
+]
