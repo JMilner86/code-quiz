bb956678d13a12125854b608164a352d69983149
Jonah Milner <jonahmilner86@gmail.com>
2022-02-11 18:29:26 -0700

    added a main section tag, began trying to get the timer to work

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,13 @@
 // Step 2, code the questions
 //Step 3, code the leader board
 
+let beginQuiz = document.querySelector("#start-quiz");
+let timerDisplay = document.createElement("p")
+
+
 const timer = () => {
     let countdown = 60
-    
+    console.log('is work')
 }
 
+beginQuiz.addEventListener("click", timer());
